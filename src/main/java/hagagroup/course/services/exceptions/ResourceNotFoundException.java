be0210fe6f665c6be730c97e358376632d2f1d82
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-14 14:17:56 -0300

    Exception Handling - findById

---
+++ src/main/java/hagagroup/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package hagagroup.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found id" + id);
+	}
+
+}
