24d533be0f415336e86865c362a15074811dc463
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-14 17:10:49 -0300

    Exception handling - delete

--- src/main/java/hagagroup/course/resources/UserResource.java
+++ src/main/java/hagagroup/course/resources/UserResource.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.config.JpaRepositoryNameSpaceHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/hagagroup/course/resources/exceptions/DataBaseException.java
@@ -0,0 +1,10 @@
+package hagagroup.course.resources.exceptions;
+
+public class DataBaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
